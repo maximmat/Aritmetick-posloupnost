b7f4ed29b163450e6e2411a362c6a3429162aabd
maximmat <maximmatous@seznam.cz>
2022-01-28 22:59:16 +0100

    Upload

---
+++ Aritmetická posloupnost.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int diference = 0, predchozi = 0;
+
+	printf("Zadejte prvni clen poslopnosti: ");
+	scanf("%d", &predchozi);
+
+	printf("\nZadejte diferenci: ");
+	scanf("%d", &diference);
+
+	printf("\n");
+
+	for (int i = 1; i <= 20; i++) {
+		printf("Clen cislo: %d: %d\n", i, predchozi);
+		predchozi += diference;
+	}
+}
